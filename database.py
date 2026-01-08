d3c39ac4a0c97bb62b9e5dfc93a0f91801f46337
mide1020r <akinrefonayomide1@gmai.com>
2026-01-08 15:26:35 +0100

    latest commit

--- database.py
+++ database.py
@@ -2,22 +2,14 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.environ["DATABASE_URL"]  
 
-if not DATABASE_URL:
-    DATABASE_URL = "postgresql://postgres:1234@127.0.0.1:5432/pizza_delivery"
-
-   
-
-    engine = create_engine(DATABASE_URL, echo=True)
-else:
-    
-    engine = create_engine(
-        DATABASE_URL,
-        connect_args={"sslmode": "require"},
-        pool_pre_ping=True,
-        echo=True,
-    )
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"sslmode": "require"},
+    pool_pre_ping=True,
+    echo=True,
+)
 
 Base = declarative_base()
 Session = sessionmaker(bind=engine)
