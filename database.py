cae1524df1f509322fd7a57f19f84b1831f8a42c
mide1020r <akinrefonayomide1@gmai.com>
2026-01-08 17:01:16 +0100

    latest commit

--- database.py
+++ database.py
@@ -2,13 +2,17 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
+
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+
 if not DATABASE_URL:
-   
+    
     DATABASE_URL = "postgresql+psycopg2://postgres:1234@127.0.0.1:5432/pizza_delivery"
 
+
 connect_args = {"sslmode": "require"} if "render.com" in DATABASE_URL else {}
+
 
 engine = create_engine(
     DATABASE_URL,
@@ -17,5 +21,10 @@ engine = create_engine(
     echo=True,
 )
 
+print("Using DATABASE_URL:", DATABASE_URL)
+
+
 Base = declarative_base()
+
+
 Session = sessionmaker(bind=engine)
